0ff75f05428471ad0417deeb756fa523dfbab982
AriadnaPuigventos <ariadnapuigventos@gmail.com>
2021-03-14 20:18:05 +0100

    changing

--- src/api/api.py
+++ src/api/api.py
@@ -1,28 +1,26 @@
+# coding=utf-8
 from flask import Flask, render_template
-#from histogram-density import​ histogramdensity
-#from hypothesis-scatter import​ hypothesisscatter
+
 print("\nData found, your API's working!!!")
 print()
 
 app = Flask(__name__)
 
-@app.route(​"/"​)
-def​ landing_page():​
-    return​ render_template(​'index.html'​)
+@app.route('/')
+def landing_page():
+    return render_template('index.html')
 
-@app.route(​"/hypothesis"​)
-def​ scatter():
-    ​return​ render_template(​'hypothesis.html'​) 
+@app.route('/hypothesis')
+def scatter():
+    return render_template('hypothesis.html')
 
-@app.route(​"/density-MMSE"​)
-def​ histogram():
-    return​ render_template(​'histogram.html'​)
+@app.route('densityMMSE')
+def histogram():
+    return render_template('histogram.html')
 
-@app.route(​"/mri-image"​)
-def​ image():
-    ​return​ render_template(​'mri-image.html'​)
-  
+@app.route('mriimage')
+def image():
+    return render_template('mriimage.html')
 
-if​ __name__ == ​'__main__'​:
-    #app.run("settings.json")
-    app.run(host=​'127.0.0.1'​,port=​6060, debug=​True​)
+if __name__ == '__main__':
+    app.run(host = '127.0.0.1', port = 6060, debug=True)
