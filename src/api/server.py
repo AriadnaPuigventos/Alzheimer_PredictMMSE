6169a1e77bb0609ee3dcd0e3e290541de4c402e6
AriadnaPuigventos <ariadnapuigventos@gmail.com>
2021-03-17 17:55:56 +0100

    update 17/03

--- src/api/server.py
+++ src/api/server.py
@@ -1,8 +1,6 @@
 # coding=utf-8
-from flask import Flask, render_template
-
-print("\nData found, your API's working!!!")
-print()
+from flask import Flask, render_template, request
+from utils.apis.py import cambios_input_datos
 
 app = Flask(__name__)
 
@@ -10,13 +8,23 @@ app = Flask(__name__)
 def landing_page():
     return render_template('index.html')
 
+
 @app.route('/hypothesis')
 def scatter():
     return render_template('hypothesis.html')
 
-@app.route('/mriimage')
+
+@app.route('/mriimage', methods=['POST', 'GET'])
 def image():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        image = request.files['filename']
+        image.save(".", "image.png")
+        X_test = image.reshape(1,-1)
+        predict = cambios_input_datos(X_test)
+        print("Status == ", predict)
     return render_template('mriimage.html')
+
 
 if __name__ == '__main__':
     app.run(host = '127.0.0.1', port = 6060, debug=True)
--- src/utils/apis.py
+++ src/utils/apis.py
@@ -1,1 +1,10 @@
 # función de predicción
+import json
+
+def cambios_input_datos(X_test):
+    """leemos csv y hacer la prediccion"""
+    path = "../models/"
+    loaded_model_CNN_C_ok = load(open(path + "best_model_CNN_C_opt_RMSprop.sav", "rb"))
+    prediction = loaded_model_CNN_C_ok.predict(X_test)
+    prediction_df = prediction_df.to_json()
+    return prediction_df #prediction
